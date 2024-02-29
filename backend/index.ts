9dc48d0ecb6b7c98dd92e66e4e4a24bc308c6440
Girish bari <root@LAPTOP-QP221T8A>
2024-02-29 16:42:00 +0530

    fix: cors prob

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ import SocketFun from "./socket/socketService";
 
 const app = express();
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true,
   optionSuccessStatus: 200
 }));
